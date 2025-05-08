e3352e28773de15d8a622b749f5d0c0896914124
Thu Yein <thuyein1996alpha@gmail.com>
2025-05-08 12:29:08 +0700

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    const name = "John";
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,23 @@
+function ListGroup() {
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    
+    return (
+        <>
+            <h1>List Group</h1>
+            <ul className="list-group">
+                {items.map((item) => (
+                    <li key={item}>{item}</li>
+                    ))}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
